9c8bd5954755d14323b00e4650bd80d437329b34
Maunsi <maukeegan@hotmail.com>
2015-09-20 12:48:15 -0300

    Test para el ejercicio 2

--- src/uba/algo3/tp2/ejercicio2/PavilionTest.java
+++ src/uba/algo3/tp2/ejercicio2/PavilionTest.java
@@ -2,21 +2,39 @@ package uba.algo3.tp2.ejercicio2;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.util.LinkedList;
 
 import org.junit.Test;
 
-public class Ejercicio2Test {
+public class PavilionTest {
 
 	@Test
-	public void test() throws IOException {
-		Ejercicio2 ej = new Ejercicio2("Tp2Ej2");
+	public void testPab1() {
+		
 		Portal p1 = new Portal(0, 10, 10, 1);
 		Pavilion pab1 = new Pavilion(11, 10);
 		pab1.setPortal(p1);
 		
-		Portal p2 = new Portal(0, 1, 1, 2);
+		Graph actual = pab1.toGraph();
+		Graph exp = new Graph(11*10 + 1);
+		for(int i = 1; i < 11*10 ; i++) // lleno las posiciones conectadas caminando
+		{
+			if(i % 10 != 0)
+			{	
+				exp.addEdge(i-1, i);
+			}
+		}
+		
+		exp.addEdge(10, 11*10);
+		exp.addEdge(101, 11*10);
+
+		assertEquals(exp, actual);
+	}
+	
+	@Test
+	public void testPab2(){
+
+		/*Portal p2 = new Portal(0, 1, 1, 2);
 		Portal p3 = new Portal(1, 2, 3, 1);
 		Portal p4 = new Portal(2, 3, 4, 0);
 		Portal p5 = new Portal(3, 4, 2, 1);
@@ -27,8 +45,13 @@ public class Ejercicio2Test {
 		portalesPab2.add(p4);
 		portalesPab2.add(p5);
 		pab2.setPortales(portalesPab2);
-		
-		Portal p6 = new Portal(0, 2, 2, 3);
+		*/
+	}
+	
+	@Test
+	public void testPab3(){
+
+		/*Portal p6 = new Portal(0, 2, 2, 3);
 		Portal p7 = new Portal(2, 4, 3, 2);
 		Portal p8 = new Portal(3, 1, 4, 5);
 		Portal p9 = new Portal(4, 3, 5, 2);
@@ -39,21 +62,7 @@ public class Ejercicio2Test {
 		portalesPab3.add(p8);
 		portalesPab3.add(p9);
 		pab3.setPortales(portalesPab3);
-		
-		LinkedList<Pavilion> ciudad = new LinkedList<Pavilion>();
-		ciudad.add(pab1);
-		ciudad.add(pab2);
-		ciudad.add(pab3);
-		assertEquals(ciudad, ej.getPavilions());
-		
+		*/
 	}
 
-	@Test
-	public void testCatedra() throws IOException {
-
-		Ejercicio2 ej = new Ejercicio2("Tp2Ej2");
-		assertEquals((Integer)21, ej.run(0));
-		assertEquals((Integer)18, ej.run(1));
-		assertEquals((Integer)17, ej.run(2));
-	}
 }
